ec06f754d567e18e917a50da25e78c33eb1ad565
Roberto Nóbrega <r_t_4@hotmail.com>
2022-04-28 14:32:01 -0300

    Projeto Atualizado 1.1

---
+++ projetoBasico1/src/main/java/aplicacao/Main.java
@@ -0,0 +1,10 @@
+package aplicacao;
+
+public class Main {
+	
+	public static void main(String[] args){  /* Método para Executar este Programa, intitulado de Programa1 */
+		Programa1 prog = new Programa1();
+		prog.executarPrograma();
+	}
+	
+}
